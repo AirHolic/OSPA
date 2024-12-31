2cbef5f2296d82366ed3d9d87db5ff134f859759
AirHolic <2670446215@qq.com>
2024-12-31 10:56:31 +0800

    添加全字匹配，提示标签，循环查找

--- searchdialog.cpp
+++ searchdialog.cpp
@@ -12,21 +12,28 @@ SearchDialog::SearchDialog(QTextEdit *textEdit, QWidget *parent)
     // 复选框
     caseSensitiveCheckBox = new QCheckBox("Case Sensitive", this);
     regexCheckBox = new QCheckBox("Regular Expression", this);
+    wholeWordCheckBox = new QCheckBox("Whole Word", this);
 
     // 按钮
     findNextButton = new QPushButton("Find Next", this);
     findPreviousButton = new QPushButton("Find Previous", this);
+
+    // 提示信息标签
+    statusLabel = new QLabel("0 of 0", this);
 
     // 布局
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
     mainLayout->addWidget(searchLineEdit);
     mainLayout->addWidget(caseSensitiveCheckBox);
     mainLayout->addWidget(regexCheckBox);
+    mainLayout->addWidget(wholeWordCheckBox);
 
     QHBoxLayout *buttonLayout = new QHBoxLayout;
     buttonLayout->addWidget(findNextButton);
     buttonLayout->addWidget(findPreviousButton);
     mainLayout->addLayout(buttonLayout);
+
+    mainLayout->addWidget(statusLabel); // 添加提示信息标签
 
     // 连接信号和槽
     connect(findNextButton, &QPushButton::clicked, this, &SearchDialog::findNext);
@@ -41,23 +48,45 @@ void SearchDialog::findNext()
         return;
     }
 
-    QTextDocument::FindFlags flags;
-    if (caseSensitiveCheckBox->isChecked()) {
-        flags |= QTextDocument::FindCaseSensitively;
-    }
-
     QTextCursor cursor = textEdit->textCursor();
+    QTextDocument::FindFlags flags = getFindFlags();
+
     if (regexCheckBox->isChecked()) {
         QRegExp regex(searchText, caseSensitiveCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
         cursor = textEdit->document()->find(regex, cursor, flags);
+    } else if (wholeWordCheckBox->isChecked()) {
+        cursor = textEdit->document()->find(searchText, cursor, flags | QTextDocument::FindWholeWords);
     } else {
         cursor = textEdit->document()->find(searchText, cursor, flags);
     }
 
     if (cursor.isNull()) {
-        QMessageBox::information(this, "Search", "No more matches found.");
+        // 搜索到最后一条，从头开始
+        cursor = textEdit->textCursor();
+        cursor.movePosition(QTextCursor::Start);
+        if (regexCheckBox->isChecked()) {
+            QRegExp regex(searchText, caseSensitiveCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+            cursor = textEdit->document()->find(regex, cursor, flags);
+        } else if (wholeWordCheckBox->isChecked()) {
+            cursor = textEdit->document()->find(searchText, cursor, flags | QTextDocument::FindWholeWords);
+        } else {
+            cursor = textEdit->document()->find(searchText, cursor, flags);
+        }
+
+        if (cursor.isNull()) {
+            QMessageBox::information(this, "Search", "No matches found.");
+            statusLabel->setText("0 of 0");
+        } else {
+            highlightText(cursor);
+            int totalMatches = findTotalMatches();
+            int currentMatchIndex = findCurrentMatchIndex(cursor);
+            statusLabel->setText(QString("%1 of %2").arg(currentMatchIndex + 1).arg(totalMatches));
+        }
     } else {
         highlightText(cursor);
+        int totalMatches = findTotalMatches();
+        int currentMatchIndex = findCurrentMatchIndex(cursor);
+        statusLabel->setText(QString("%1 of %2").arg(currentMatchIndex + 1).arg(totalMatches));
     }
 }
 
@@ -69,23 +98,45 @@ void SearchDialog::findPrevious()
         return;
     }
 
-    QTextDocument::FindFlags flags = QTextDocument::FindBackward;
-    if (caseSensitiveCheckBox->isChecked()) {
-        flags |= QTextDocument::FindCaseSensitively;
-    }
-
     QTextCursor cursor = textEdit->textCursor();
+    QTextDocument::FindFlags flags = getFindFlags() | QTextDocument::FindBackward;
+
     if (regexCheckBox->isChecked()) {
         QRegExp regex(searchText, caseSensitiveCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
         cursor = textEdit->document()->find(regex, cursor, flags);
+    } else if (wholeWordCheckBox->isChecked()) {
+        cursor = textEdit->document()->find(searchText, cursor, flags | QTextDocument::FindWholeWords);
     } else {
         cursor = textEdit->document()->find(searchText, cursor, flags);
     }
 
     if (cursor.isNull()) {
-        QMessageBox::information(this, "Search", "No more matches found.");
+        // 搜索到第一条，从末尾开始
+        cursor = textEdit->textCursor();
+        cursor.movePosition(QTextCursor::End);
+        if (regexCheckBox->isChecked()) {
+            QRegExp regex(searchText, caseSensitiveCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+            cursor = textEdit->document()->find(regex, cursor, flags);
+        } else if (wholeWordCheckBox->isChecked()) {
+            cursor = textEdit->document()->find(searchText, cursor, flags | QTextDocument::FindWholeWords);
+        } else {
+            cursor = textEdit->document()->find(searchText, cursor, flags);
+        }
+
+        if (cursor.isNull()) {
+            QMessageBox::information(this, "Search", "No matches found.");
+            statusLabel->setText("0 of 0");
+        } else {
+            highlightText(cursor);
+            int totalMatches = findTotalMatches();
+            int currentMatchIndex = findCurrentMatchIndex(cursor);
+            statusLabel->setText(QString("%1 of %2").arg(currentMatchIndex + 1).arg(totalMatches));
+        }
     } else {
         highlightText(cursor);
+        int totalMatches = findTotalMatches();
+        int currentMatchIndex = findCurrentMatchIndex(cursor);
+        statusLabel->setText(QString("%1 of %2").arg(currentMatchIndex + 1).arg(totalMatches));
     }
 }
 
@@ -96,4 +147,84 @@ void SearchDialog::highlightText(const QTextCursor &cursor)
     extra.format.setBackground(Qt::yellow); // 高亮背景色为黄色
     textEdit->setTextCursor(cursor);
     textEdit->setExtraSelections(QList<QTextEdit::ExtraSelection>() << extra);
+}
+
+QTextDocument::FindFlags SearchDialog::getFindFlags() const
+{
+    QTextDocument::FindFlags flags;
+    if (caseSensitiveCheckBox->isChecked()) {
+        flags |= QTextDocument::FindCaseSensitively;
+    }
+    return flags;
+}
+
+int SearchDialog::findTotalMatches() const
+{
+    QString searchText = searchLineEdit->text();
+    if (searchText.isEmpty()) {
+        return 0;
+    }
+
+    QTextDocument::FindFlags flags = getFindFlags();
+    QTextCursor cursor(textEdit->document());
+    int totalMatches = 0;
+
+    if (regexCheckBox->isChecked()) {
+        QRegExp regex(searchText, caseSensitiveCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+        while (!(cursor = textEdit->document()->find(regex, cursor, flags)).isNull()) {
+            totalMatches++;
+        }
+    } else if (wholeWordCheckBox->isChecked()) {
+        while (!(cursor = textEdit->document()->find(searchText, cursor, flags | QTextDocument::FindWholeWords)).isNull()) {
+            totalMatches++;
+        }
+    } else {
+        while (!(cursor = textEdit->document()->find(searchText, cursor, flags)).isNull()) {
+            totalMatches++;
+        }
+    }
+
+    return totalMatches;
+}
+
+int SearchDialog::findCurrentMatchIndex(const QTextCursor &currentCursor) const
+{
+    QString searchText = searchLineEdit->text();
+    if (searchText.isEmpty()) {
+        return -1;
+    }
+
+    QTextDocument::FindFlags flags = getFindFlags();
+    QTextCursor cursor(textEdit->document());
+    int currentMatchIndex = -1;
+    int index = 0;
+
+    if (regexCheckBox->isChecked()) {
+        QRegExp regex(searchText, caseSensitiveCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+        while (!(cursor = textEdit->document()->find(regex, cursor, flags)).isNull()) {
+            if (cursor == currentCursor) {
+                currentMatchIndex = index;
+                break;
+            }
+            index++;
+        }
+    } else if (wholeWordCheckBox->isChecked()) {
+        while (!(cursor = textEdit->document()->find(searchText, cursor, flags | QTextDocument::FindWholeWords)).isNull()) {
+            if (cursor == currentCursor) {
+                currentMatchIndex = index;
+                break;
+            }
+            index++;
+        }
+    } else {
+        while (!(cursor = textEdit->document()->find(searchText, cursor, flags)).isNull()) {
+            if (cursor == currentCursor) {
+                currentMatchIndex = index;
+                break;
+            }
+            index++;
+        }
+    }
+
+    return currentMatchIndex;
 }
--- searchdialog.h
+++ searchdialog.h
@@ -9,6 +9,7 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QMessageBox>
+#include <QLabel>
 
 class SearchDialog : public QDialog
 {
@@ -26,10 +27,15 @@ private:
     QLineEdit *searchLineEdit; // 搜索输入框
     QCheckBox *caseSensitiveCheckBox; // 大小写敏感复选框
     QCheckBox *regexCheckBox; // 正则表达式复选框
+    QCheckBox *wholeWordCheckBox; // 全字匹配复选框
     QPushButton *findNextButton; // 查找下一个按钮
     QPushButton *findPreviousButton; // 查找上一个按钮
+    QLabel *statusLabel; // 提示信息标签
 
     void highlightText(const QTextCursor &cursor);
+    QTextDocument::FindFlags getFindFlags() const;
+    int findTotalMatches() const; // 查找总匹配数
+    int findCurrentMatchIndex(const QTextCursor &cursor) const; // 查找当前匹配项的位置
 };
 
 #endif // SEARCHDIALOG_H
